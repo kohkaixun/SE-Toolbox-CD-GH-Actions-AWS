bc6b5dae46fe5905ae20f98cd1e9206d9e4cdbfe
kohkaixun <kkx1929@gmail.com>
2023-10-13 15:40:12 +0800

    佛祖保佑，永无bug

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use('/api/addresses', require('./routes/addressRoutes')) // add
 
 // optional
 app.get('/', (req, res) => {
-  res.status(200).json("Welcome to Address Book with CD!")
+  res.status(200).json("Welcome to Address Book with CD!佛祖保佑，永无bug！")
 })
 
 app.listen(port, () => {
