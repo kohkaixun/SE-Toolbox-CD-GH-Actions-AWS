f25a1fa4c3ef45fc4624b1dd310f687630f8a403
kohkaixun <kkx1929@gmail.com>
2023-10-13 17:50:40 +0800

    New permission added in IAM

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use('/api/addresses', require('./routes/addressRoutes')) // add
 
 // optional
 app.get('/', (req, res) => {
-  res.status(200).json("Welcome to Address Book with CD! 佛祖保佑，永无bug！")
+  res.status(200).json("Welcome to Address Book with CD! 我佛慈悲，佛祖保佑，永无bug！")
 })
 
 app.listen(port, () => {
